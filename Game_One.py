9ced3c0eabe6586a5a6d80d5acff0b380d4b3a88
oltreblu <malidilias@gmail.com>
2025-07-10 00:47:54 +0200

    Update Game_One.py
    i'm a dumbass and forgot about draws

--- Game_One.py
+++ Game_One.py
@@ -1,6 +1,6 @@
 import Board as board_import
 import Check_Game as check
-def main(game_to_play_on, board_to_play_on):
+def main(game_to_play_on, board_to_play_on, turn):
     board_column_index = [2, 6, 10] #These are indexes of where the cross, x and o are going to be
     board_row = [1, 3, 5, 7] #These are indexes of where the cross, x and o are going to be
     coordinates_player1 = user_input(1, game_to_play_on)
@@ -12,6 +12,10 @@ def main(game_to_play_on, board_to_play_on):
     for row in board_to_play_on:
         print(row) #Prints the Board
     is_won = check.main(game_to_play_on) #Checks if the game was already won
+    if (2 * turn) + 1 == 9:
+        print("Oh no! IT'S A DRAW!")
+        is_won = True
+        return game_to_play_on, board_to_play_on, is_won
     if is_won:
         return game_to_play_on, board_to_play_on, is_won
     coordinates_player2 = user_input(2, game_to_play_on)
@@ -34,11 +38,15 @@ def user_input(player, game):
             coordinates_player = [int(i) for i in coordinates_player]
             if game[coordinates_player[0] - 1][coordinates_player[1] - 1] == 1 or game[coordinates_player[0] - 1][coordinates_player[1] - 1] == 2:
             #Converts coordinates by player into the coordinates of the game, then checks if it was already written once
-                print("ERROR! THERE IS ALREADY A CROSS/CIRCLE THERE! TRY AGAIN!")
+                print("ERROR! THERE IS ALREADY A CROSS OR A CIRCLE THERE! TRY AGAIN!")
+                continue
+            elif len(coordinates_player) != 2:
+                print("ERROR! INPUT 2 NUMBERS! TRY AGAIN")
                 continue
             break
-        except ValueError:
+        except:
             print("ERROR! INPUT NOT VALID! RETRY!")
+            continue
     return coordinates_player
 
 if __name__ == "__main__":
@@ -47,4 +55,4 @@ if __name__ == "__main__":
         initial_board[i] = initial_board[i].replace("   "," 0 ")
     for row1 in initial_board:
         print(row1)
-    main([[0, 0, 0],[0, 0, 0],[0, 0, 0]],initial_board)
+    main([[0, 0, 0],[0, 0, 0],[0, 0, 0]],initial_board, 0)
