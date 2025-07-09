4c8ec12f3693f2eb18b07a7940ddc7786bd15567
oltreblu <malidilias@gmail.com>
2025-07-09 21:54:03 +0200

    Update Game_One.py
    Made it a little bit more readable

--- Game_One.py
+++ Game_One.py
@@ -3,17 +3,7 @@ import Check_Game as check
 def main(game_to_play_on, board_to_play_on):
     board_column_index = [2, 6, 10] #These are indexes of where the cross, x and o are going to be
     board_row = [1, 3, 5, 7] #These are indexes of where the cross, x and o are going to be
-    while True:
-        try:
-            coordinates_player1 = input('PLAYER 1: INPUT THE COORDINATES IN THE FORMAT (integer from 1 to 3) "row,column": ').strip().split(",")
-            coordinates_player1 = [int(i) for i in coordinates_player1]
-            if game_to_play_on[coordinates_player1[0] - 1][coordinates_player1[1] - 1] == 1 or game_to_play_on[coordinates_player1[0] - 1][coordinates_player1[1] - 1] == 2:
-            #Converts coordinates by player into the coordinates of the game, then checks if it was already written once
-                print("ERROR! THERE IS ALREADY A CROSS OR A CIRCLE THERE! TRY AGAIN!")
-                continue
-            break
-        except ValueError:
-            print("ERROR! INPUT NOT VALID! RETRY!")
+    coordinates_player1 = user_input(1, game_to_play_on)
     game_to_play_on[coordinates_player1[0] - 1][coordinates_player1[1] - 1] = 1 #Converts coordinates by player into the coordinates of the game
     for game_row in range(3):
         for game_column in range(3):
@@ -24,16 +14,7 @@ def main(game_to_play_on, board_to_play_on):
     is_won = check.main(game_to_play_on) #Checks if the game was already won
     if is_won:
         return game_to_play_on, board_to_play_on, is_won
-    while True:
-        try:
-            coordinates_player2 = input('PLAYER 2: INPUT THE COORDINATES IN THE FORMAT (integer from 1 to 3) "row,column": ').strip().split(",")
-            coordinates_player2 = [int(i) for i in coordinates_player2]
-            if game_to_play_on[coordinates_player2[0] - 1][coordinates_player2[1] - 1] == 1 or game_to_play_on[coordinates_player2[0] - 1][coordinates_player2[1] - 1] == 2:
-                print("ERROR! THERE IS ALREADY A CROSS OR A CIRCLE THERE! TRY AGAIN!")
-                continue
-            break
-        except ValueError:
-            print("ERROR! INPUT NOT VALID! RETRY!")
+    coordinates_player2 = user_input(2, game_to_play_on)
     game_to_play_on[coordinates_player2[0] - 1][coordinates_player2[1] - 1] = 2
     for game_row in range(3):
         for game_column in range(3):
@@ -42,8 +23,24 @@ def main(game_to_play_on, board_to_play_on):
     for row in board_to_play_on:
         print(row)
     return game_to_play_on, board_to_play_on, is_won
+
 def odd(n): #A simple function for odd numbers
     return [i for i in range(n) if i % 2 != 0]
+
+def user_input(player, game):
+    while True:
+        try:
+            coordinates_player = input(f'PLAYER {player}: INPUT THE COORDINATES IN THE FORMAT (integer from 1 to 3) "row,column": ').strip().split(",")
+            coordinates_player = [int(i) for i in coordinates_player]
+            if game[coordinates_player[0] - 1][coordinates_player[1] - 1] == 1 or game[coordinates_player[0] - 1][coordinates_player[1] - 1] == 2:
+            #Converts coordinates by player into the coordinates of the game, then checks if it was already written once
+                print("ERROR! THERE IS ALREADY A CROSS OR A CIRCLE THERE! TRY AGAIN!")
+                continue
+            break
+        except ValueError:
+            print("ERROR! INPUT NOT VALID! RETRY!")
+    return coordinates_player
+
 if __name__ == "__main__":
     initial_board = board_import.board_stock(3)
     for i in odd(7):
