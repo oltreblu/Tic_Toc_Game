e07055eeceeb9d9a893b782181425dfba6967ec0
oltreblu <malidilias@gmail.com>
2025-07-14 19:23:20 +0200

    Update Tic_Tac_Toe_Game.py
    Made it more readable

--- Tic_Tac_Toe_Game.py
+++ Tic_Tac_Toe_Game.py
@@ -1,18 +1,24 @@
 import Game_One as game
 import Check_Game as check
 import Board as board_import
+
 def main():
     sol = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
     board = board_import.board_stock(3) #Creates a board to play on
+
     for i in game.odd(7):
         board[i] = board[i].replace("   "," 0 ") #Initializing the board to be played on Tic-Tac-Toe
+
     for row1 in board:
         print(row1) #Prints the first blank board
+
     for turn in range(5):
         sol, board, is_won_midgame = game.main(sol, board, turn) #Starts the game
+
         if is_won_midgame: #Checks if it was won mid-game
             return
         is_won = check.main(sol) #Checks if it was won at the end
+
         if is_won:
             return
     print("Oh no! It's a Draw!")
