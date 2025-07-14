010502a0a7dcd76f459d2cc13a4d9893bab5118a
oltreblu <malidilias@gmail.com>
2025-07-14 19:22:58 +0200

    Update Check_Game.py
    Made it more readable.

--- Check_Game.py
+++ Check_Game.py
@@ -1,21 +1,26 @@
 def main(game_to_check):
     diag = []
     diag_2 = []
+
     for index in range(3):
         line = game_to_check[index]
         column = [row[index] for row in game_to_check]
         diag.append(game_to_check[index][index])
         diag_2.append(game_to_check[index][2 - index])
+
         for player in range(1,3):
             if line.count(player) == 3 or column.count(player) == 3:
                 print(f"Congratulations! Player {player} WON!!")
                 return True
+
     for player in range(1,3):
         if diag.count(player) == 3 or diag_2.count(player) == 3:
             print(f"Congratulations! Player {player} WON!!")
             return True
+
     else:
         return False
-game_test = [[2, 1, 1], [2, 1, 0], [2, 0, 1]]
+
 if __name__ == "__main__":
+    game_test = [[2, 1, 1], [2, 1, 0], [2, 0, 1]]
     main(game_test)
