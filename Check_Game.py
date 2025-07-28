1933558568a47a9b31306d37a629059710b2ee7c
oltreblu <malidilias@gmail.com>
2025-07-28 18:56:10 +0200

    Update Check_Game.py

--- Check_Game.py
+++ Check_Game.py
@@ -1,23 +1,36 @@
 def check(game_to_check):
     diagonal = []
     reverse_diagonal = []
+    players = [1, 2]
 
     for index in range(3):
         line = game_to_check[index]
         column = [row[index] for row in game_to_check]
         diagonal.append(game_to_check[index][index])
         reverse_diagonal.append(game_to_check[index][2 - index])
-        players = [1,2]
 
         for player in players:
             if line.count(player) == 3 or column.count(player) == 3:
                 print(f"Congratulations! Player {player} WON!!")
-                return True
-            elif diagonal.count(player) == 3 or reverse_diagonal.count(player) == 3:
-                print(f"Congratulations! Player {player} WON!!")
-                return True
-    return False
+                return player
+
+    for player in players:
+        if diagonal.count(player) == 3 or reverse_diagonal.count(player) == 3:
+            print(f"Congratulations! Player {player} WON!!")
+            return player
+
+
+    line_count = 0
+    for line in game_to_check:
+        if 0 not in line:
+            line_count += 1
+
+    if line_count == 3:
+        print(f"Oh no! It's a DRAW!!")
+        return -1
+
+    return 0
 
 if __name__ == "__main__":
-    game_test = [[1, 2, 1], [2, 2, 1], [2, 2, 1]]
+    game_test = [[1, 2, 1], [2, 1, 1], [2, 1, 2]]
     check(game_test)
