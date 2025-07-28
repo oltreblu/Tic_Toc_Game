2240e9cbce93be4dd860475f48e72f0028b2c73e
oltreblu <malidilias@gmail.com>
2025-07-28 18:57:37 +0200

    Update Tic_Tac_Toe_Game.py

--- Tic_Tac_Toe_Game.py
+++ Tic_Tac_Toe_Game.py
@@ -1,24 +1,45 @@
-from Game_One import main as game
 from Check_Game import check
+from Game_One import main as game
 from Board import board_stock as board
 
 def main():
-    sol = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
-    game_board = board(3) #Creates a board to play on
+    scores = {'player1': 0, 'player2': 0}
+    while True:
+        try:
+            max_score = int(input(f"Please Tell me the max score you want to play: "))
+            break
+        except ValueError:
+            print("ERROR! NOT VALID INPUT! TRY AGAIN!")
 
-    for row1 in game_board:
-        print(row1) #Prints the first blank board
-
-    for turn in range(5):
-        sol, game_board, is_won_midgame = game(sol, game_board, turn) #Starts the game
-
-        if is_won_midgame: #Checks if it was won mid-game
-            return
-        is_won = check(sol) #Checks if it was won at the end
-
-        if is_won:
-            return
-
-    print("Oh no! It's a Draw!")
+    while True:
+        game_matrix = []
+        for line in range(3):
+            game_matrix.append([0,0,0])
+        game_board = board(3)
+        for row in game_board:
+            print(row)  # Prints the Board
+        is_won = 0
+        while is_won == 0:
+            for player in [1,2]:
+                game_matrix, game_board = game(game_matrix, game_board, player)
+                is_won = check(game_matrix)
+                if is_won == 0:
+                    continue
+                elif is_won == -1:
+                    break
+                else:
+                    scores['player' + str(is_won)] += 1
+                    break
+        print("-" * 50)
+        print(f"The score of Player 1 is: {scores['player1']}")
+        print(f"The score of Player 2 is: {scores['player2']}")
+        print("-" * 50)
+        for player, score in scores.items():
+            if score == max_score:
+                if player == 'player1':
+                    print("Congratulations! Player 1 WON!!!")
+                else:
+                    print("Congratulations! Player 2 WON!!!")
+                return
 
 main()
