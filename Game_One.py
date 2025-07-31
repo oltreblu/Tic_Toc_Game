cb4753dcdc4fa1edf1465a7fc264c91862e98666
oltreblu <malidilias@gmail.com>
2025-07-31 12:48:56 +0200

    Update Game_One.py

--- Game_One.py
+++ Game_One.py
@@ -1,16 +1,13 @@
-from Board import board_stock as board
-from Check_Game import check
-
 def main(game_to_play_on, board_to_play_on, player):
-    coordinates_player = user_input(1, game_to_play_on)
+    coordinates_player = user_input(player, game_to_play_on)
     game_to_play_on[coordinates_player[0] - 1][coordinates_player[1] - 1] = player #Converts coordinates by player into the coordinates of the game
 
     board_to_play_on = next_board(game_to_play_on, board_to_play_on) #Creates the next board
 
     for row in board_to_play_on:
         print(row) #Prints the Board
-
     return game_to_play_on, board_to_play_on
+
 def user_input(player, game):
     while True:
         try:
@@ -27,38 +24,29 @@ def user_input(player, game):
             if restart_while:
                 continue
 
-            elif game[coordinates_player[0] - 1][coordinates_player[1] - 1] == 1 or game[coordinates_player[0] - 1][coordinates_player[1] - 1] == 2:
-                #Converts coordinates by player into the coordinates of the game, then checks if it was already written once
+            elif (game[coordinates_player[0] - 1][coordinates_player[1] - 1] == 1
+                  or game[coordinates_player[0] - 1][coordinates_player[1] - 1] == 2):
+                #Converts coordinates by player into the coordinates of the game, then checks if it was already written
                 print("ERROR! THERE IS ALREADY A CROSS OR A CIRCLE THERE! TRY AGAIN!")
                 continue
 
             elif len(coordinates_player) != 2:
                 print("ERROR! INPUT 2 NUMBERS! TRY AGAIN")
                 continue
-
             break
 
         except:
             print("ERROR! INPUT NOT VALID! RETRY!")
-
     return coordinates_player
 
 def next_board(game_matrix, board_to_modify):
-    board_column_index = [2, 6, 10] #These are indexes of where the cross, x and o are going to be
-    board_row = [1, 3, 5] #These are indexes of where the cross, x and o are going to be
+    board_column_index = [2, 6, 10] #These are columns of where the cross, x and o are going to be
+    board_row = [1, 3, 5] #These are rows of where the cross, x and o are going to be
 
     for game_row in range(3):
         for game_column in range(3):
             board_to_modify[board_row[game_row]] = board_to_modify[board_row[game_row]][:board_column_index[game_column]] + str(game_matrix[game_row][game_column]) + board_to_modify[board_row[game_row]][board_column_index[game_column] + 1:]
+            #Replaces the blank spaces with 1, 2 or 0.
             board_to_modify[board_row[game_row]] = board_to_modify[board_row[game_row]].replace("1", "X").replace("2", "O").replace("0", " ")
 
     return board_to_modify
-
-
-if __name__ == "__main__":
-    initial_board = board(3)
-
-    for row1 in initial_board:
-        print(row1)
-
-    main([[0, 0, 0],[0, 0, 0],[0, 0, 0]],initial_board, 0)
